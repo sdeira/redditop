e175f4d687761204be8cd8fd3fca73a72587141c
Sebas Deira <deira8@hotmail.com>
2020-10-23 20:17:42 -0300

    Add post detail fragment

--- app/src/main/java/com/example/redditop/ui/postdetail/PostDetailFragment.kt
+++ app/src/main/java/com/example/redditop/ui/postdetail/PostDetailFragment.kt
@@ -1,10 +1,24 @@
 package com.example.redditop.ui.postdetail
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.redditop.databinding.PostDetailFragmentBinding
 
 /**
  * A fragment representing a single Post detail screen.
  */
 class PostDetailFragment : Fragment() {
+    private lateinit var binding: PostDetailFragmentBinding
 
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = PostDetailFragmentBinding.inflate(inflater, container, false)
+        return binding.root
+    }
 }
