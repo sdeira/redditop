58a8ed4b5fa92ff130468cb61ec05f92c5cb3aa3
Sebas Deira <deira8@hotmail.com>
2020-10-23 17:20:19 -0300

    Add created at text in post row

--- app/src/main/java/com/example/redditop/ui/bindingadapters/PostsBindingAdapter.kt
+++ app/src/main/java/com/example/redditop/ui/bindingadapters/PostsBindingAdapter.kt
@@ -2,9 +2,11 @@ package com.example.redditop.ui.bindingadapters
 
 import android.view.View
 import android.widget.ImageView
+import android.widget.TextView
 import androidx.databinding.BindingAdapter
 import com.bumptech.glide.Glide
 import com.example.redditop.R
+import java.util.concurrent.TimeUnit
 
 @BindingAdapter("loadImageFromUrl")
 fun loadImageFromUrl(view: ImageView, thumbnail: String?) {
@@ -19,5 +21,34 @@ fun loadImageFromUrl(view: ImageView, thumbnail: String?) {
             view.visibility = View.GONE
             Glide.with(view.context).clear(view)
         }
+    }
+}
+
+@BindingAdapter("createdTime")
+fun createdTime(view: TextView, created: Long?) {
+    created?.let {
+        val currentTime = System.currentTimeMillis()
+        val createInMillis = TimeUnit.SECONDS.toMillis(created)
+        val diff = currentTime - createInMillis
+        val days = TimeUnit.MILLISECONDS.toDays(diff)
+        val hours = TimeUnit.MILLISECONDS.toHours(diff)
+        val minutes = TimeUnit.MILLISECONDS.toMinutes(diff)
+        val seconds = TimeUnit.MILLISECONDS.toSeconds(diff)
+        var createdAt = ""
+        when {
+            days != 0L -> {
+                createdAt = view.resources.getString(R.string.days, days.toString())
+            }
+            hours != 0L -> {
+                createdAt = view.resources.getString(R.string.hours, hours.toString())
+            }
+            minutes != 0L -> {
+                createdAt = view.resources.getString(R.string.minutes, minutes.toString())
+            }
+            seconds != 0L -> {
+                createdAt = view.resources.getString(R.string.seconds, seconds.toString())
+            }
+        }
+        view.text = createdAt
     }
 }
