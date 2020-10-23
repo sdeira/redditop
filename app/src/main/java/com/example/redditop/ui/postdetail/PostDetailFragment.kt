501426ee7b93049223f5cc6b6206d549f2ea0fbf
Sebas Deira <deira8@hotmail.com>
2020-10-23 12:14:10 -0300

    rearrange proyect structure

---
+++ app/src/main/java/com/example/redditop/ui/postdetail/PostDetailFragment.kt
@@ -0,0 +1,10 @@
+package com.example.redditop.ui.postdetail
+
+import androidx.fragment.app.Fragment
+
+/**
+ * A fragment representing a single Post detail screen.
+ */
+class PostDetailFragment : Fragment() {
+
+}
