016886c1e8c9a004e9f2f8d11bae926c2ad1abe9
Sebas Deira <deira8@hotmail.com>
2020-10-24 11:08:33 -0300

    Add download image functionality

---
+++ app/src/main/java/com/example/redditop/Extensions.kt
@@ -0,0 +1,71 @@
+package com.example.redditop
+
+import android.content.ContentValues
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
+import android.os.Build
+import android.os.Environment
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.Toast
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
+import java.io.File
+import java.io.FileOutputStream
+import java.io.IOException
+import java.io.OutputStream
+
+const val QUALITY = 100
+
+fun String.saveImage(context: Context, name: String) {
+    Glide.with(context)
+        .asBitmap()
+        .load(this)
+        .into(object : CustomTarget<Bitmap>() {
+            override fun onLoadCleared(placeholder: Drawable?) {
+                //Nothing to do here
+            }
+
+            override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                saveImage(context, resource, name)
+            }
+        })
+}
+
+private fun saveImage(context: Context, bitmap: Bitmap, name:String) {
+    try {
+        val fos: OutputStream?
+        val directoryName = "Redditop"
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
+            val resolver = context.contentResolver
+            val contentValues = ContentValues()
+            contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, name)
+            contentValues.put(MediaStore.MediaColumns.MIME_TYPE, "image/png")
+            contentValues.put(MediaStore.MediaColumns.RELATIVE_PATH, "DCIM/$directoryName")
+            val imageUri = resolver.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+            fos = imageUri?.let { resolver.openOutputStream(it) }
+        } else {
+            val imagesDir = Environment.getExternalStorageDirectory().toString() + File.separator + directoryName
+            val file = File(imagesDir)
+            if (!file.exists()) {
+                file.mkdir()
+            }
+            val image = File(imagesDir, "$name.png")
+            fos = FileOutputStream(image)
+        }
+        fos?.let {
+            val saved = bitmap.compress(Bitmap.CompressFormat.PNG, QUALITY, fos)
+            if (saved) {
+                Toast.makeText(context, context.resources.getString(R.string.image_downloaded), Toast.LENGTH_LONG).show()
+            }
+            it.flush()
+            it.close()
+        }
+    } catch (exception: IOException) {
+        Log.i("Fail to save Image", exception.toString())
+        Toast.makeText(context, context.resources.getString(R.string.image_downloaded_fails), Toast.LENGTH_LONG).show()
+    }
+
+}
--- app/src/main/java/com/example/redditop/ui/postdetail/PostDetailFragment.kt
+++ app/src/main/java/com/example/redditop/ui/postdetail/PostDetailFragment.kt
@@ -1,17 +1,23 @@
 package com.example.redditop.ui.postdetail
 
+import android.Manifest
+import android.content.pm.PackageManager
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.findNavController
 import androidx.navigation.fragment.navArgs
 import androidx.paging.ExperimentalPagingApi
+import com.example.redditop.R
 import com.example.redditop.databinding.PostDetailFragmentBinding
 import com.example.redditop.model.UiModel
+import com.example.redditop.saveImage
 import com.example.redditop.viewmodel.PostsViewModel
 import kotlinx.coroutines.launch
 
@@ -20,6 +26,9 @@ import kotlinx.coroutines.launch
  */
 @ExperimentalPagingApi
 class PostDetailFragment : Fragment() {
+    companion object {
+        const val PERMISSION_REQUEST_CODE = 22222
+    }
     private lateinit var binding: PostDetailFragmentBinding
     private val viewModel: PostsViewModel by activityViewModels()
     private val args: PostDetailFragmentArgs by navArgs()
@@ -30,12 +39,23 @@ class PostDetailFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         binding = PostDetailFragmentBinding.inflate(inflater, container, false)
+
+        initPostItem()
+        initShowBack()
+        initDownloadImage()
+        return binding.root
+    }
+
+    private fun initPostItem() {
         lifecycleScope.launch {
             val post = viewModel.getPost(args.postName)
             post?.let {
                 binding.postItem = UiModel.PostItem(post)
             }
         }
+    }
+
+    private fun initShowBack() {
         if (args.showBack) {
             binding.detailToolbar.setNavigationOnClickListener {view ->
                 view.findNavController().navigateUp()
@@ -43,6 +63,52 @@ class PostDetailFragment : Fragment() {
         } else {
             binding.detailToolbar.navigationIcon = null
         }
-        return binding.root
+    }
+
+    private fun initDownloadImage() {
+        binding.download.setOnClickListener {
+            if (hasPermission()) {
+                binding.postItem?.let {
+                    context?.apply {
+                        it.post.thumbnail?.saveImage(this, it.post.name)
+                    }
+                }
+
+            } else {
+                requestPermissions(arrayOf(android.Manifest.permission.WRITE_EXTERNAL_STORAGE),
+                    PERMISSION_REQUEST_CODE)
+            }
+        }
+    }
+
+    private fun hasPermission(): Boolean {
+        context?.let { context ->
+            val permission = ContextCompat.checkSelfPermission(context,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+            return permission == PackageManager.PERMISSION_GRANTED
+        }
+        return false
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        when (requestCode) {
+            PERMISSION_REQUEST_CODE -> {
+                context?.apply {
+                    if (grantResults.isEmpty() || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
+                        Toast.makeText(this, resources.getString(R.string.download_image_without_permissions),
+                            Toast.LENGTH_LONG).show()
+                    } else {
+                        binding.postItem?.let {
+                            it.post.thumbnail?.saveImage(this, it.post.name)
+                        }
+                    }
+                }
+            }
+        }
     }
 }
